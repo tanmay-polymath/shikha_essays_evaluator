23dac4375b9ef8ae316844d86acf5da6c37b43d9
Varun <varunmukherjee5@gmail.com>
2024-01-03 14:23:21 +0530

    gpt model update

--- app/api/evaluate/route.ts
+++ app/api/evaluate/route.ts
@@ -91,7 +91,7 @@ export async function POST(request: NextRequest) {
       ]
 
     const res = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo-1106",
       stream: true,
       // temperature: 0.3,
       messages: [
